e2e9b733e4bb0d81df126103bdc60a1e5f093d88
foroa <audrey.foro@gmail.com>
2021-11-28 15:09:23 +0100

    newCV nov21

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -25,17 +25,16 @@ const Presentation = () => {
 
                 <div className="right-area">
                     <div className="txt">
-                    Actuellement étudiante en master de compétences complémentaires en informatique, je recherche un stage de fin d’études en développement logiciel au sein d’une entreprise dans laquelle je pourrai à la fois monter en compétences, relever des défis et apporter une valeur ajoutée de par mon profil pluridisciplinaire. 
+                    Prochainement diplômé d’un master de compétences complémentaires en informatique, je suis actuellement à la recherche d’un poste d’ingénieur en développement logiciel au sein d’une entreprise dans laquelle je pourrai à la fois renforcer mes compétences, relever des défis et apporter une valeur ajoutée de par mon profil pluridisciplinaire.  
                     </div>
-                    <div className="txt">Diplômée en neurosciences, j’ai choisi de compléter ma formation de base en biologie avec d’autres disciplines afin d’avoir une connaissance plus large des sciences du cerveaux et ses méthodes d’étude, et par la même, d’affiner mon projet professionnel. Mes expériences en laboratoire de recherche m’ont permis d’acquérir rigueur, 
-                    persévérance et autonomie. Également, de développer un goût pour la programmation et l’utilisation d’outils informatiques. Par ailleurs, mes années d’études post-master et mon séjour Erasmus m’ont permis de développer mon ouverture d’esprit, mon adaptabilité, mes capacités de travail en équipe et le goût de la coopération.
+                    <div className="txt">Un bagage en neurosciences et plusieurs expériences en laboratoire de recherche m’ont permis d’acquérir rigueur, persévérance et autonomie, également, un goût pour la programmation informatiques. Par ailleurs, un séjour Erasmus m’a permis de développer mon ouverture d’esprit, mon adaptabilité et mes capacités de travail en équipe. Aujourd’hui, je souhaite poursuivre professionnellement dans l’informatique afin de m’investir sur dans des projets industriels et/ou R&D. 
                     </div>
-                    <div className="txt">
+                    {/* <div className="txt">
                     Aujourd’hui, je souhaite poursuivre professionnellement dans l’informatique afin de m’investir sur des projets industriels et/ou R&amp;D, en développement back et front.  
-                    </div>
+                    </div> */}
                     <div className="cv">
-                        <a href="./doc/CV_AudreyForo_FR_detaille_stage.pdf" target="blank" className="hover">CV en français</a>
-                        <a href="./doc/CV_AudreyForo_ENG_detailed_internship.pdf" target="blank" className="hover">CV in english</a>
+                        <a href="./doc/CV_AudreyForo_Dev_2022_longversion_FR.pdf" target="blank" className="hover">CV en français</a>
+                        <a href="./doc/CV_AudreyForo_Dev_2022_longversion_ENG.pdf" target="blank" className="hover">CV in english</a>
                     </div>
                 </div>
 
@@ -60,16 +59,16 @@ const Presentation = () => {
 
                 <div className="right-area">
                     <div className="txt">
-                    Student in a master of complementary skills in computer science, I am currently looking for an internship in software development. I aspire to gain new skills, meet new challenges and adding value to a company with my interdisciplinary profile.  
+                    Soon graduated in computer science skills, I am currently looking for a software engineer/developer position within a company where I would be able to strengthen my skills, take up challenges and bring value with my multidisciplinary profile.   
                     </div>
-                    <div className="txt">Following graduation in neurosciences, I chose to enrich my background in biology with other disciplines in order to broaden my knowledge of brain science and its methods of study. My professional project got fined-tuned in the process. Besides, I gained discipline, perseverance, autonomy and a keen IT interest from internships in research labs. Furthermore, my post-master experiences and Erasmus exchange brought me a more open mindset, adaptability and teamwork skills. 
+                    <div className="txt">A background in neuroscience and several experiences in research labs enabled me to acquire rigour, perseverance and autonomy, also, a keen taste in programming. Furthermore, an Erasmus exchange brought me open-mindedness, adaptability and teamwork skills. Today, I want to pursue a computer science career by getting involved in industrial and/or R&D projects.  
                     </div>
-                    <div className="txt">
+                    {/* <div className="txt">
                     Nowadays, I want to pursue professionally in the computer science field in order to invest in projects in back and front development.
-                    </div>
+                    </div> */}
                     <div className="cv">
-                        <a href="./doc/CV_AudreyForo_ENG_detailed_internship.pdf" target="blank" className="hover">CV in english</a>
-                        <a href="./doc/CV_AudreyForo_FR_detaille_stage.pdf" target="blank" className="hover">CV en français</a>
+                        <a href="./doc/CV_AudreyForo_Dev_2022_longversion_ENG.pdf" target="blank" className="hover">CV in english</a>
+                        <a href="./doc/CV_AudreyForo_Dev_2022_longversion_FR.pdf" target="blank" className="hover">CV en français</a>
                     </div>
                 </div>
 
